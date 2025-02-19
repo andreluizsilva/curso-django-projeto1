17ea1e11e8cb5ed163a2cd190899f70bf9ce31b2
Andre Silva <andre.luiz.s@outlook.com>
2025-02-18 21:25:46 -0300

    Atualizacao Models

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Recipe(models.Model):
+    title = models.CharField(max_length=65)
+    description = models.TextField(max_length=165)
+    slug = models.SlugField()
+    prepation_time = models.IntegerField()
+    prepation_time_unit = models.CharField(max_length=65)
+    servings = models.IntegerField()
+    servings_unit = models.CharField(max_length=65)
+    preparation_steps = models.TextField()
+    preparation_steps_is_html = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=False)
+    cover = models.ImageField(upload_to='recipes/covers/%y/%m/%d/')
